32572a8970ca4135e40f7f472ad97168920c7ac3
alagroy- <alagroy-@student.42.fr>
2022-06-10 18:01:01 +0200

    For Christ sake, this party is over

--- p1/k3s.sh
+++ p1/k3s.sh
@@ -4,15 +4,16 @@ sed -i 's/mirrorlist/#mirrorlist/g' /etc/yum.repos.d/CentOS-*
 sed -i 's|#baseurl=http://mirror.centos.org|baseurl=http://vault.centos.org|g' /etc/yum.repos.d/CentOS-*
 
 echo Yum fixed, installing ifconfig
+sudo systemctl disable firewalld --now
 
 sudo yum update && sudo yum install -y net-tools
 
 echo Installing K3s
 
-curl -sfL https://get.k3s.io | sh -s - server --flannel-iface=eth1
+curl -sfL https://get.k3s.io | sh -s - server --flannel-iface=eth1 --disable traefik --disable servicelb
 
 TOKEN_FILE='/var/lib/rancher/k3s/server/node-token'
 
-sudo scp -o StrictHostKeyChecking=accept-new -i /home/vagrant/.ssh/id_rsa $TOKEN_FILE adricristi@192.168.42.1:~/Inception-of-things/p1/node-token
+sudo scp -o StrictHostKeyChecking=accept-new -i /home/vagrant/.ssh/id_rsa $TOKEN_FILE alagroy-@192.168.42.1:/tmp/node-token
 
 echo Installation complete
--- p1/k3sworker.sh
+++ p1/k3sworker.sh
@@ -4,14 +4,14 @@ sed -i 's/mirrorlist/#mirrorlist/g' /etc/yum.repos.d/CentOS-*
 sed -i 's|#baseurl=http://mirror.centos.org|baseurl=http://vault.centos.org|g' /etc/yum.repos.d/CentOS-*
 
 echo Yum fixed, installing ifconfig
+sudo systemctl disable firewalld --now
 
 sudo yum update && sudo yum install -y net-tools
-
 echo Installing K3s
 
 SERVER_IP=192.168.42.110
 
-scp -o StrictHostKeyChecking=accept-new -i /home/vagrant/.ssh/id_rsa adricristi@192.168.42.1:~/Inception-of-things/p1/node-token . 
-curl -sfL https://get.k3s.io | K3S_URL=https://$SERVER_IP:6443 K3S_TOKEN=`sudo cat node-token` sh -s - --flannel-iface=eth1
+scp -o StrictHostKeyChecking=accept-new -i /home/vagrant/.ssh/id_rsa alagroy-@192.168.42.1:/tmp/node-token . 
+curl -sfL https://get.k3s.io | K3S_URL="https://$SERVER_IP:6443" K3S_TOKEN=`sudo cat node-token` sh -s - --flannel-iface=eth1
 
 echo Installation complete
